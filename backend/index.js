b1514594da7ebe369ad526a8c193906f7a907691
langford <langfordquarshie21@gmail.com>
2025-07-17 19:01:43 +0000

    chore: refactors

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,8 @@
-import { AssemblyAI } from "assemblyai";
 import dotenv from "dotenv";
 import express from "express";
-import ffmpegPath from "ffmpeg-static";
-import ffmpeg from "fluent-ffmpeg";
 import http from "http";
-import { PassThrough, Readable } from "stream";
 import { WebSocketServer } from "ws";
-
-ffmpeg.setFfmpegPath(ffmpegPath);
+import { handleWebSocketConnection } from "./utils/ws.js";
 
 dotenv.config({ path: ".env" });
 
@@ -18,57 +13,9 @@ const wss = new WebSocketServer({ server, path: "/ws" });
 
 wss.on("connection", async (ws) => {
   console.log("client connected");
-
-  const client = new AssemblyAI({ apiKey: process.env.ASSEMBLY_AI_API_KEY });
-
-  const transcriber = client.streaming.transcriber({
-    sampleRate: 16_000,
-    formatTurns: true,
-  });
-
-  transcriber.on("open", ({ id }) => {
-    console.log(`session opened -> ${id}`);
-  });
-
-  transcriber.on("error", (error) => {
-    console.error("Error:", error);
-  });
-
-  transcriber.on("close", (code, reason) =>
-    console.log("Session closed:", code, reason),
-  );
-
-  transcriber.on("turn", (turn) => {
-    if (!turn.transcript) return;
-    ws.send(JSON.stringify({ transcript: turn.transcript }));
-  });
-
-  await transcriber.connect();
-
-  const inputStream = new PassThrough();
-
-  const ffmpegProcess = ffmpeg()
-    .input(inputStream)
-    .inputFormat("webm")
-    .audioFrequency(16000)
-    .audioChannels(1)
-    .audioCodec("pcm_s16le")
-    .format("s16le")
-    .on("error", (err) => console.error("FFmpeg error:", err))
-    .pipe();
-
-  Readable.toWeb(ffmpegProcess).pipeTo(transcriber.stream());
-
-  ws.on("message", (data) => {
-    inputStream.write(data);
-  });
-
-  ws.on("close", async () => {
-    inputStream.end();
-    await transcriber.close();
-  });
+  handleWebSocketConnection(ws);
 });
 
-app.get("/", (req, res) => res.send(`server running`));
+app.get("/", (res) => res.send(`server running`));
 
 server.listen(PORT, () => console.log(`listening on http://localhost:${PORT}`));
---
+++ backend/utils/ws.js
@@ -0,0 +1,56 @@
+import { AssemblyAI } from "assemblyai";
+import ffmpegPath from "ffmpeg-static";
+import ffmpeg from "fluent-ffmpeg";
+import { PassThrough, Readable } from "stream";
+
+ffmpeg.setFfmpegPath(ffmpegPath);
+
+export async function handleWebSocketConnection(ws) {
+  const client = new AssemblyAI({ apiKey: process.env.ASSEMBLY_AI_API_KEY });
+
+  const transcriber = client.streaming.transcriber({
+    sampleRate: 16_000,
+    formatTurns: true,
+  });
+
+  transcriber.on("open", ({ id }) => {
+    console.log(`session opened -> ${id}`);
+  });
+
+  transcriber.on("error", (error) => {
+    console.error("Error:", error);
+  });
+
+  transcriber.on("close", (code, reason) =>
+    console.log("Session closed:", code, reason),
+  );
+
+  transcriber.on("turn", (turn) => {
+    if (!turn.transcript) return;
+    ws.send(JSON.stringify({ transcript: turn.transcript }));
+  });
+
+  await transcriber.connect();
+  const inputStream = new PassThrough();
+
+  const ffmpegProcess = ffmpeg()
+    .input(inputStream)
+    .inputFormat("webm")
+    .audioFrequency(16000)
+    .audioChannels(1)
+    .audioCodec("pcm_s16le")
+    .format("s16le")
+    .on("error", (err) => console.error("ffmpeg error:", err))
+    .pipe();
+
+  Readable.toWeb(ffmpegProcess).pipeTo(transcriber.stream());
+
+  ws.on("message", (data) => {
+    inputStream.write(data);
+  });
+
+  ws.on("close", async () => {
+    inputStream.end();
+    await transcriber.close();
+  });
+}
